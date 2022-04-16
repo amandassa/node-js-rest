56b089708e0fbfc7eae7f4f3f80e2592b74a14a6
amandassa <amandassa504@gmail.com>
2022-04-15 22:58:03 -0300

    Conexão com o banco de dados

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,3 +1,8 @@
 module.exports = app => {
     app.get("/atendimentos", (req, res) => res.send("Você está na página de atendimentos <3"));
+
+    app.post("/atendimentos", (req, res) => {
+        console.log(req.body);
+        res.send("Você está na rota de atendimentos.\nEstá realizando um post.");
+    });
 };
--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const customExpress = require("./config/customExpress");
-const app = customExpress();
-app.listen(3000, () => console.log("servidor rodando na porta 3000"));
+const conexao = require("./infraestrutura/conexao.js");
+const tabelas = require("./infraestrutura/tabelas.js");
+conexao.connect(err => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("conectado com sucesso");
+        tabelas.init(conexao);
+        const app = customExpress();
+        app.listen(3000, () => console.log("servidor rodando na porta 3000"));
+    }
+});
